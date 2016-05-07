75b409aeb9f743fd8f7b846fb9df0b1474c48359
kingson4wu <kingson_wu@163.com.cn>
2016-05-07 22:37:15 +0800

    Hessian RPC

--- RPC-framework/src/main/java/app/demo/BasicClient.java
+++ RPC-framework/src/main/java/app/demo/BasicClient.java
@@ -1,5 +1,6 @@
 package app.demo;
 
+import app.demo2.BasicAPI2;
 import com.caucho.hessian.client.HessianProxyFactory;
 
 public class BasicClient {
@@ -7,8 +8,10 @@ public class BasicClient {
         //String url = "http://127.0.0.1:8080/Hessian/hello";
         String url = "http://localhost:8080/EasyHessian/hello";
         HessianProxyFactory factory = new HessianProxyFactory();
-        BasicAPI basic = (BasicAPI) factory.create(BasicAPI.class, url);
+        //BasicAPI basic = (BasicAPI) factory.create(BasicAPI.class, url);
+        BasicAPI2 basic = (BasicAPI2) factory.create(BasicAPI2.class, url);
         System.out.println("Hello:" + basic.hello());
+        System.out.println("Hello:" + basic.getUser());
         System.out.println("Hello:" + basic.getUser().getUserName());
         System.out.println("Hello:" + basic.getUser().getPassword());
         basic.setGreeting("HelloGreeting");
---
+++ RPC-framework/src/main/java/app/demo2/BasicAPI2.java
@@ -0,0 +1,13 @@
+package app.demo2;
+
+//import app.demo.User;
+import app.demo2.User2;
+
+public interface BasicAPI2 {
+    
+    public void setGreeting(String greeting);
+
+    public String hello();
+
+    public User2 getUser();
+}
