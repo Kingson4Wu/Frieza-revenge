db87c09464f2c19ac1d21e45c300554ee5595695
Kingson4Wu <kingson_wu@163.com>
2017-03-13 21:23:51 +0800

    haha

--- jdk-lib/src/main/java/com/kxw/basic/SerialVersionUID/SerialVersionUIDTest.java
+++ jdk-lib/src/main/java/com/kxw/basic/SerialVersionUID/SerialVersionUIDTest.java
@@ -14,13 +14,24 @@ public class SerialVersionUIDTest {
         System.out.println(0x5dec9293e0874139L);
         //6767945503855821113 跟idea生成的一致 //jdk1.8.0_77 windows
         // jdk1.8.0_77 windows 与 zulu jdk1.8.0_102 windows 一致
+        // mac jdk1.8.0_60 一致
 
         System.out.println(0xe404a21fe304be8aL);
         //-2016308475318714742 跟idea生成的一致 //jdk1.6.0_45 windows
 
+        //windows winhex ,mac Hex Fiend 查看,类名之后的256个字节就是SerialVersionUID
 
         SerialVersionUIDClass clazz = new SerialVersionUIDClass();
         writeObjectToFile(clazz);
+
+        // 使用serialver 命令查看
+        /**
+         * ➜  classes git:(master) ✗ serialver com.kxw.basic.SerialVersionUID.SerialVersionUIDClass
+         *    com.kxw.basic.SerialVersionUID.SerialVersionUIDClass:    private static final long serialVersionUID = 6767945503855821113L;
+         * ➜  classes git:(master) ✗ pwd
+         *     /Users/kingsonwu/Personal/github/Utils4Java/jdk-lib/target/classes
+         * ➜  classes git:(master) ✗
+         */
 
     }
 
